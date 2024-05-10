8c40ddf422825285b20fe03f0688b8be3635e9c1
Jonathan vera <167922770+jonathanavl@users.noreply.github.com>
2024-05-10 10:29:15 +0000

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Generadordeexcusas
+Ejercicio Generador de excusas
---
+++ app.js
@@ -0,0 +1,19 @@
+
+let who = ['El perro','Mi abuela','Mi tortuga','Mi canario', 'Mi gato'];
+let action = ['Se comio', 'orino', 'aplasto', 'rompio'];
+let what = ['mi tarea','mi telefono','el coche'];
+let when = ['antes de clases','justo cuando terminé','mientras almorzaba','mientras yo rezaba'];
+
+function excuseGenerator(who, action, what, when){
+    numwho= Math.floor(Math.random() * who.length);
+    numaction = Math.floor(Math.random() * action.length);
+    numwhat = Math.floor(Math.random() * what.length);
+    numwhen = Math.floor(Math.random() * when.length);
+    return who[numwho] + " " + action[numaction] + " " + what[numwhat] + " " + when[numwhen];
+}
+function onLoad(){
+    var excusa = document.getElementById('excusas');
+    excusa.innerHTML = excuseGenerator(who, what,action, when);
+}
+window.onload = onLoad;
+
